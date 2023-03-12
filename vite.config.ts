591bfe48bffd61d44dc66c0972d08324a1235c12
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-12 23:34:01 +0200

    squash!

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [solidPlugin()],
-
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	// prevent vite from obscuring rust errors
 	clearScreen: false,
-	// tauri expects a fixed port, fail if that port is not available
 	server: {
 		port: 1420,
 		strictPort: true,
 	},
-	// to make use of `TAURI_DEBUG` and other env variables
-	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
 	envPrefix: ["VITE_", "TAURI_"],
 	build: {
 		target:
