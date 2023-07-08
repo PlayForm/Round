e09749d0cf4823a53e6e2d016ba02b7e9e2a23ac
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-08 16:12:10 +0300

    squash!

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { listen } from "@tauri-apps/api/event";
 import { appWindow } from "@tauri-apps/api/window";
-import type { Component } from "solid-js";
+import { listen } from "@tauri-apps/api/event";
 import { createSignal, For } from "solid-js";
+import type { Component } from "solid-js";
 import { render } from "solid-js/web";
 
 import "./assets/css/Window.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [solidPlugin()],
