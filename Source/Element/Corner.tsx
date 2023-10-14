c32646fa85888a2734cf2d9208a033dfaeb6c03f
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 13:31:37 +0300

    squash!

--- Source/Element/Corner.tsx
+++ Source/Element/Corner.tsx
@@ -1,9 +1,4 @@
-import type { Settings } from "../Option/Index.js";
-
 import "../Asset/CSS/Corner.css";
-
-import { listen } from "@tauri-apps/api/event";
-import { createSignal, mergeProps } from "solid-js";
 
 declare global {
 	interface Window {
@@ -11,20 +6,26 @@ declare global {
 	}
 }
 
-const [size, setSize] = createSignal(window.settings.size);
-
-await listen(
-	"size",
-	async (event: { payload: { message: { Size: number } } }) => {
-		setSize(event.payload.message.Size);
-	}
+export const Size = (await import("solid-js")).createSignal(
+	window.settings.size
 );
+
+await (
+	await import("@tauri-apps/api/event")
+).listen("size", async (event: { payload: { message: { Size: number } } }) => {
+	Size[1](event.payload.message.Size);
+});
 
 // biome-ignore lint/suspicious/noExplicitAny:
-export default (props: any) => (
+export default async (Property: any) => (
 	<div
 		class="Corner"
-		data-corner={mergeProps({ id: "Default" }, props).id}
-		style={{ "--corner-size": `${size()}px` }}
+		data-corner={
+			(await import("solid-js")).mergeProps({ id: "Default" }, Property)
+				.id
+		}
+		style={{ "--corner-size": `${Size[0]()}px` }}
 	/>
 );
+
+import type { Settings } from "../Option/Index.js";
--- Source/Index.tsx
+++ Source/Index.tsx
@@ -1,29 +1,38 @@
-import { listen } from "@tauri-apps/api/event";
-import { appWindow } from "@tauri-apps/api/window";
-import { For, createSignal } from "solid-js";
-import { render } from "solid-js/web";
 import "./Asset/CSS/Window.css";
-import Corner from "./Element/Corner.jsx";
 
-const [mode, setMode] = createSignal(window.settings.mode);
-
-await appWindow.setIgnoreCursorEvents(true);
-
-await listen(
-	"mode",
-	async (event: { payload: { message: { Mode: string } } }) => {
-		setMode(event.payload.message.Mode);
-	}
+export const Mode = (await import("solid-js")).createSignal(
+	window.settings.mode
 );
 
-const Window = () => {
-	return (
-		<div class="Window" data-label={appWindow.label} data-mode={mode()}>
+export const {
+	default: {
+		appWindow: { setIgnoreCursorEvents, label },
+	},
+}: {
+	default: { appWindow: WebviewWindow };
+} = await import("@tauri-apps/api/window");
+
+await setIgnoreCursorEvents(true);
+
+await (
+	await import("@tauri-apps/api/event")
+).listen("mode", async (event: { payload: { message: { Mode: string } } }) => {
+	Mode[1](event.payload.message.Mode);
+});
+
+(await import("solid-js/web")).render(
+	() => (
+		<div class="Window" data-label={label} data-mode={Mode[0]()}>
 			<For each={["BottomLeft", "BottomRight", "TopLeft", "TopRight"]}>
 				{(corner: string) => <Corner id={corner} />}
 			</For>
 		</div>
-	);
-};
+	),
+	document.getElementById("window") as HTMLElement
+);
 
-render(() => <Window />, document.getElementById("window") as HTMLElement);
+export const { default: Corner } = await import("./Element/Corner.jsx");
+
+export const { For } = await import("solid-js");
+
+import type { WebviewWindow } from "@tauri-apps/api/window";
